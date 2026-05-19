08e63b002448c2dc863a0c8c75fb650f8f919d7d
Tory <okmijn95123578@163.com>
2026-05-19 19:55:44 +0800

    Delete fe-Lolita-glitcher.lua

--- fe-Lolita-glitcher.lua
+++
@@ -1,7 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Jskfhggjxu/My-Script/refs/heads/main/check-acc-cmds.lua"))()
-      
-      if not hasReset or serverEvent then
-          return
-      else
-          loadstring(game:HttpGet("https://raw.githubusercontent.com/Jskfhggjxu/My-Script/refs/heads/main/fe-Lolita-glitcher%20(1).lua"))()
-end
