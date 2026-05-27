aa4a0069f2f2e9a1e6ac9ad0b1e35c46359b4c90
Tory <okmijn95123578@163.com>
2026-05-27 17:36:37 +0800

    Update check-acc-cmds.lua

--- check-acc-cmds.lua
+++ check-acc-cmds.lua
@@ -1,7 +1,16 @@
 local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GroupService = game:GetService("GroupService")
 local localPlayer = Players.LocalPlayer
+
+local function promptGroup()
+    task.spawn(function()
+        pcall(function()
+            GroupService:PromptGroupJoinAsync(localPlayer, 34901800)
+        end)
+    end)
+end
 
 local serverEvent = ReplicatedStorage:WaitForChild("01_server", 1)
 
@@ -9,21 +18,21 @@ if not serverEvent then
     pcall(function()
         StarterGui:SetCore("SendNotification", {
             Title = "NOTE",
-            Text = "YOU'RE NOT ON JUST A BASEPLATE EXECUTE SCRIPT!!!!111",
+            Text = "Youre not on just a baseplate execute script!",
             Duration = 5
         })
     end)
-    return false -- 没有找到事件，直接返回失败
+    promptGroup()
+    return false
 end
 
--- 发送重置命令
 local args = {"cmd", "-rs"}
 serverEvent:FireServer(unpack(args))
 
 local hasReset = false
-local isChecked = false -- 用来标记是否检测完毕
-
+local isChecked = false
 local connection
+
 connection = localPlayer.CharacterAdded:Connect(function(newCharacter)
     hasReset = true
     isChecked = true
@@ -31,7 +40,7 @@ connection = localPlayer.CharacterAdded:Connect(function(newCharacter)
     pcall(function()
         StarterGui:SetCore("SendNotification", {
             Title = "NOTE",
-            Text = "Your account can use cmds!",
+            Text = "checked,Loading...",
             Duration = 5
         })
     end)
@@ -48,7 +57,17 @@ task.delay(0.5, function()
         pcall(function()
             StarterGui:SetCore("SendNotification", {
                 Title = "NOTE",
-                Text = "YOUR ACCOUNT CANT USE CMDS!!!!111 PLS CHOOSE YOUR ACCOUNT",
+                Text = "Your account cant use cmds!",
+                Duration = 5
+            })
+        end)
+        
+        task.wait(0.1)
+        
+        pcall(function()
+            StarterGui:SetCore("SendNotification", {
+                Title = "NOTE",
+                Text = "Join game group to use script!",
                 Duration = 5
             })
         end)
@@ -57,6 +76,7 @@ task.delay(0.5, function()
             connection:Disconnect()
             connection = nil
         end
+        promptGroup()
     end
 end)
 
